d3145ddcd1ee12c669ae06a7a150558147be396a
Sam Devine <samuel.will.devine@gmail.com>
2022-01-15 17:38:16 -0700

    setup forecasts controller for GET request

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
-    lat_lng = GeocodeFacade.get_lat_long(params[:location])
+    lat_long = GeocodeFacade.get_lat_long(params[:location])
+    forecast = WeatherFacade.get_weather_data(lat_long[:lat], lat_long[:lng])
+
+    render json: WeatherSerializer.new(forecast)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :forecast, only: :show
+      get '/forecast', to: 'forecasts#show'
     end
   end
 end
