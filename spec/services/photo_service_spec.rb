ccc7027b9ad4b881434b238f66ced99c37bad42f
Sam Devine <samuel.will.devine@gmail.com>
2022-01-16 13:50:32 -0700

    check for attribution info in service

--- spec/services/photo_service_spec.rb
+++ spec/services/photo_service_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe PhotoService do
       expect(payload[:results].length).to eq 1
       expect(payload[:results].first).to have_key :urls
       expect(payload[:results].first[:urls]).to have_key :raw
+      expect(payload[:results].first).to have_key :user
+      expect(payload[:results].first[:user]).to have_key :name
+      expect(payload[:results].first[:user]).to have_key :links
+      expect(payload[:results].first[:user][:links]).to have_key :html
     end
   end
 end
