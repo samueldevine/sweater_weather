a2846516f8f1629e11e17920f9ae04ec047ae766
Sam Devine <samuel.will.devine@gmail.com>
2022-01-16 12:49:46 -0700

    update facade to use weather poro

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe WeatherFacade do
-  describe '::get_lat_long' do
-    it 'returns only the lat and lng for a given location', :vcr do
+  describe '::get_weather_data' do
+    it 'returns a Weather object', :vcr do
       lat = 39.738453
       long = -104.984853
-      response = WeatherFacade.get_weather_data(lat, long)
+      weather = WeatherFacade.get_weather_data(lat, long)
 
-      expect(response[:current][:dt]).to be_a Time
-      expect(response[:current][:sunrise]).to be_a Time
-      expect(response[:current][:sunset]).to be_a Time
-      expect(response[:daily].first[:dt]).to be_a Time
-      expect(response[:daily].first[:sunrise]).to be_a Time
-      expect(response[:daily].first[:sunset]).to be_a Time
-      expect(response[:hourly].first[:dt]).to be_a Time
+      expect(weather).to be_an_instance_of Weather
+      expect(weather.current_weather).to be_a Hash
+      expect(weather.daily_weather).to be_an Array
+      expect(weather.daily_weather.length).to eq 5
+      expect(weather.hourly_weather.length).to eq 8
     end
   end
 end
