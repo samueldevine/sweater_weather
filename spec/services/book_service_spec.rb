2b3a67d7f02ea303f0d98b6312e85bac797f80e8
Sam Devine <samuel.will.devine@gmail.com>
2022-01-18 09:36:39 -0700

    book service spec

---
+++ spec/services/book_service_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe BookService do
+  describe '::conn' do
+    it 'creates a Faraday connection' do
+      expect(BookService.conn).to be_a Faraday::Connection
+    end
+  end
+
+  describe '::get_books', :vcr do
+    it 'returns all books with titles matching the query' do
+      payload = BookService.get_books('Denver,CO')
+
+      expect(payload).to be_a Hash
+      expect(payload).to have_key :numFound
+      expect(payload).to have_key :docs
+      expect(payload[:docs].length).to eq payload[:numFound]
+
+      # skipping the first book in my test because no isbn is listed.
+      # will add some logic to account for this possibility in my facade
+      payload[:docs][1..5].each do |book|
+        expect(book).to have_key :isbn
+        expect(book).to have_key :title
+        expect(book).to have_key :publisher
+      end
+    end
+  end
+end
