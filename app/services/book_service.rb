76a5e6aff91988ef435daf33567c9e4237885df9
Sam Devine <samuel.will.devine@gmail.com>
2022-01-18 09:37:00 -0700

    feat: book service

---
+++ app/services/book_service.rb
@@ -0,0 +1,19 @@
+class BookService
+  class << self
+    def conn
+      Faraday.new('http://openlibrary.org/')
+    end
+
+    def parse_json(url, params)
+      JSON.parse(conn.get(url, params).body, symbolize_names: true)
+    end
+
+    def get_books(location)
+      url = '/search.json'
+      params = {
+        title: location
+      }
+      parse_json(url, params)
+    end
+  end
+end
